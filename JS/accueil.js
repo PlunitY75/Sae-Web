99434eb4d4fd515a620dba1fbb2b546d4e2338a4
PlunitY75 <115430052+PlunitY75@users.noreply.github.com>
2023-10-08 13:31:04 +0200

    Add files via upload

--- JS/accueil.js
+++ JS/accueil.js
@@ -11,3 +11,19 @@ menu.onclick = () => {
   menu.classList.toggle("bx-x");
   navbar.classList.toggle("open");
 };
+
+let sectionS = document.querySelectorAll('section');
+
+window.onscroll = () => {
+    sectionS.forEach(sec => {
+      let top = window.scrollY;
+      let offset = sec.offsetTop - 175;
+      let height = sec.offsetHeight;
+
+      if(top + 500 >= offset && top  < offset + height + 100)
+        sec.classList.add('show-animate');
+      else 
+        sec.classList.remove('show-animate');
+    
+    })
+}
