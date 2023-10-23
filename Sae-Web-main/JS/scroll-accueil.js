e26ca4c1fdcccb8b2a2d7e07ef37abaac7c72249
Lucasquette <115700977+Lucasquette@users.noreply.github.com>
2023-10-23 09:19:36 +0200

    Delete Sae-Web-main directory

--- Sae-Web-main/JS/scroll-accueil.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const scrollButton = document.getElementById("scrollBouton");
-
-  scrollButton.addEventListener("click", function () {
-    const section1 = document.getElementById("TA-actualités");
-    section1.scrollIntoView({ behavior: "smooth" });
-  });
-});
