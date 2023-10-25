72c3c844849f067f01e8efa26d540488a261732c
Lucasquette <115700977+Lucasquette@users.noreply.github.com>
2023-10-25 14:25:06 +0200

    Add files via upload

---
+++ JS/popup-contact.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+
+    document.querySelector("#button").addEventListener("click", () => {
+        const popupEnvoie = document.getElementById("#popup")
+        debugger
+
+        if(popupEnvoie){
+            popupEnvoie.classList("active")
+        }
+    } )
+
+    document.getElementById()
+
+}
